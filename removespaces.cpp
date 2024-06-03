6b7cdb3aea1420097fd77eac6ca8253922c9bdf6
Vanshrana18 <155249843+Vanshrana18@users.noreply.github.com>
2024-06-03 23:25:02 +0530

    Update removespaces.cpp

--- removespaces.cpp
+++ removespaces.cpp
@@ -4,7 +4,10 @@ string remove(string s){
     string temp="";
     for(int i =0;i<s.length();i++){
         if(s[i]==' '){
-        temp.push_back("ok");
+        temp.push_back('a');
+            temp.push_back('4');
+temp.push_back('0');
+        
         
         }
         else{
